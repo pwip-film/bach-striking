973610881784185e3415fabacab7bcd89e8ab270
Aaron Baker <adbaker125@gmail.com>
2016-12-08 14:58:59 -0500

    textarea styling update

--- wp-content/themes/cafe/functions.php
+++ wp-content/themes/cafe/functions.php
@@ -128,7 +128,7 @@ function html5blank_styles()
     // wp_register_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), '4.5.0' );
     // wp_enqueue_style('font-awesome');
 
-    wp_register_style('bach-styles', get_template_directory_uri() . '/style.css', array(), '1.3', 'all');
+    wp_register_style('bach-styles', get_template_directory_uri() . '/style.css', array(), '1.4', 'all');
     wp_enqueue_style('bach-styles'); // Enqueue it!
 }
 
